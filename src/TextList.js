d776c8f47184f319d74395c3ce0a99b0faffaa3b
peterPalVanyo <vanyo.peter.pal@gmail.com>
2019-11-13 18:01:40 +0100

    fetch and display text

--- src/TextList.js
+++ src/TextList.js
@@ -1,10 +1,38 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
+import axios from "axios";
 
 class TextList extends Component {
+  static defaultProps = {
+    numOfTexts: 5
+  };
+  constructor(props) {
+    super(props);
+    this.state = { texts: [] };
+  }
 
-    render() {
-        return <h1>from the list</h1>
+  async componentDidMount() {
+    //_id, content, author
+    const texts = [];
+    while (texts.length < this.props.numOfTexts) {
+      let resp = await axios.get("https://api.quotable.io/random");
+      texts.push(resp.data.content);
     }
+    this.setState({texts: texts})
+  }
+
+  render() {
+    return (
+      <div className='TextList'>
+        <h1>Text to evaluate</h1>
+        <div className='TextList-texts'>
+            {this.state.texts.map(text => (
+                <div>{text}</div>
+            ))}
+        </div>
+
+      </div>
+    );
+  }
 }
 
-export default TextList
+export default TextList;
