fed3fe9f0766727ce70ba2891037161c7239a7f5
peterPalVanyo <vanyo.peter.pal@gmail.com>
2019-11-14 04:42:28 +0100

    textlist first style round

--- src/TextList.js
+++ src/TextList.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
+import rate from "./image/rate.svg";
+import "./TextList.css";
 
 class TextList extends Component {
   static defaultProps = {
@@ -17,19 +19,24 @@ class TextList extends Component {
       let resp = await axios.get("https://api.quotable.io/random");
       texts.push(resp.data.content);
     }
-    this.setState({texts: texts})
+    this.setState({ texts: texts });
   }
 
   render() {
     return (
-      <div className='TextList'>
-        <h1>Text to evaluate</h1>
-        <div className='TextList-texts'>
-            {this.state.texts.map(text => (
-                <div>{text}</div>
-            ))}
+      <div className="TextList">
+        <div className="TextList-side">
+          <h1 className="TextList-title">
+            <span>Rate</span> Them!
+          </h1>
+          <object type="image/svg+xml" data={rate}></object>
+          <button className="TextList-getmore">New Texts</button>
         </div>
-
+        <div className="TextList-texts">
+          {this.state.texts.map(text => (
+            <div>{text}</div>
+          ))}
+        </div>
       </div>
     );
   }
